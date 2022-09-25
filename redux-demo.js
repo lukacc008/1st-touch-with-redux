a536a09e18131bd7e4e920cf50f2281a9dc6beb0
unknown <lukacc008@gmail.com>
2022-09-25 16:09:55 +0200

    _

--- redux-demo.js
+++ redux-demo.js
@@ -28,4 +28,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'decrement' });
+store.dispatch({ type: 'decrement' }); 
